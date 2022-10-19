6ced8c411839a4b6115dceb43b87c0057048c129
otobongadaise <otobongadaise@yahoo.com>
2022-10-19 02:51:13 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by otobong
+ * Return: 0
+ */
+
+int main(void)
+{
+_purtchar('_');
+_purtchar('p');
+_purtchar('u');
+_purtchar('t');
+_purtchar('c');
+_purtchar('h');
+_purtchar('a');
+_purtchar('r');
+_purtchar('\n');
+return (0);
+}
