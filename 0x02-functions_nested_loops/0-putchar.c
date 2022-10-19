949d5c2cde88fc9b4f1aac9d7b660746e42e0029
otobongadaise <otobongadaise@yahoo.com>
2022-10-19 03:27:50 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-_purtchar('_');
-_purtchar('p');
-_purtchar('u');
-_purtchar('t');
-_purtchar('c');
-_purtchar('h');
-_purtchar('a');
-_purtchar('r');
-_purtchar('\n');
-return (0);
+	_purtchar('_');
+	_purtchar('p');
+	_purtchar('u');
+	_purtchar('t');
+	_purtchar('c');
+	_purtchar('h');
+	_purtchar('a');
+	_purtchar('r');
+	_purtchar('\n');
+	return (0);
 }
