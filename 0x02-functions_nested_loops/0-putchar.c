21269971d216c20b0b5c816a2c226767bb138128
otobongadaise <otobongadaise@yahoo.com>
2022-10-19 05:38:44 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
- * Return: Always 0
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	_purtchar('_');
-	_purtchar('p');
-	_purtchar('u');
-	_purtchar('t');
-	_purtchar('c');
-	_purtchar('h');
-	_purtchar('a');
-	_purtchar('r');
-	_purtchar('\n');
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 	return (0);
 }
