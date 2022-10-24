9e3829bee155fe41c898decd2e8b085a5a1d3ae9
otobongadaise <otobongadaise@yahoo.com>
2022-10-24 06:46:59 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,14 +2,14 @@
 
 /**
  * puts2 - prints one character out of two
- * @str: input of string
+ * @str: input
  * Return: print
  */
 void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i--)
+	for (; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
 			_putchar(str[i]);
